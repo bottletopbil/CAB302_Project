80a13436102fe3a467d3c52aecd54eadbfe2a20a
ashim <n11580275@qut.edu.au>
2024-09-17 18:00:16 +1000

    started on the login screen ui and buttons, as well as text entry(but no data stored yet as db hasnt been setup)
    added line of code for Refactored email validation

--- src/main/java/com/example/addressbook/LoginController.java
+++ src/main/java/com/example/addressbook/LoginController.java
@@ -9,7 +9,7 @@ import javafx.scene.control.TextField;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.stage.Stage;
-
+import javafx.scene.control.Alert;
 import java.io.IOException;
 
 public class LoginController {
