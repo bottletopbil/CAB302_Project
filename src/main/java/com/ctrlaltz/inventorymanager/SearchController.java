49daa97aa684bab0a7a4bf534eb2793ebcb518ed
obtoonie <spamjrc123@gmail.com>
2024-10-19 14:38:33 +1000

    added barebones search screen based on items view

---
+++ src/main/java/com/ctrlaltz/inventorymanager/SearchController.java
@@ -0,0 +1,12 @@
+package com.ctrlaltz.inventorymanager;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
+import java.io.IOException;
+
+public class SearchController {
+    @FXML private TextField searchField;
+}
