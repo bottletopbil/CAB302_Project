4f21a7130c3d8cb37f433066501d1f38d71f8551
Minh Bui <minh@abi-international.com.au>
2024-10-26 11:59:46 +1000

    feature: missing unit tests from personal branch

--- src/test/java/com/ctrlaltz/inventorymanager/TagTest.java
+++ src/test/java/com/ctrlaltz/inventorymanager/TagTest.java
@@ -3,20 +3,32 @@ package com.ctrlaltz.inventorymanager;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class TagTest {
     Tag tag;
-
+    Item item;
+    TagDB tagDb;
+    ItemTagDB itemTagDb;
     @BeforeEach
     void setUp() {
+        float testPrice = 1.99F;
+        item = new Item(5, 1, "Test Item", "Test Brand", testPrice, "1 Year", 1, "New", "", "Test Description", "1234", "Australia", "26/10/2024", "26/10/2024");
         tag = new Tag("TestTag", "This is a test tag");
+        tagDb = new TagDB();
+        tagDb.initializeTable();
+        tagDb.insert(tag);
+        itemTagDb = new ItemTagDB();
+        itemTagDb.initializeTable();
     }
 
     @Test
     void setName() {
         String newName = "TestTag2";
         tag.setName(newName);
+        tagDb.update(tag);
         assertEquals(newName, tag.getName());
     }
 
@@ -24,6 +36,16 @@ class TagTest {
     void setDesc() {
         String newDesc = "Testing setting a new tag description";
         tag.setDesc(newDesc);
+        tagDb.update(tag);
         assertEquals(newDesc, tag.getDesc());
+    }
+
+    @Test
+    void tagItem() {
+        itemTagDb.removeTag(item, tag);
+        itemTagDb.tagItem(item, tag);
+        List<Item> items = itemTagDb.getItemsByTag(tag);
+        Item retrievedItem = items.get(0);
+        assertEquals(item.getName(), retrievedItem.getName());
     }
 }
